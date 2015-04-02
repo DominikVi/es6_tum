a76e58ddf3dd1f4c7b8ad896bfd7a7d384a61462
Jonathan Kienzle <jonathan.kienzle@gmail.com>
2015-04-02 13:18:07 +0200

    use requirejs as module system

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,3 @@
-const a = 2;
+export function init() {
+
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel'),
+    bower = require('gulp-bower'),
     concat = require('gulp-concat'),
     del = require('del');
 
@@ -17,12 +18,35 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('dist/js'));
 });
 
+// Html
+gulp.task('html', function () {
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+// Bower
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('dist/lib'));
+});
+
 // Clean
 gulp.task('clean', function(cb) {
-    del(['dist/js'], cb);
+    del(['dist/js', '*.html'], cb);
 });
 
 // Default task
 gulp.task('default', ['clean'], function() {
-  gulp.start('scripts');
+  gulp.start('scripts', 'html', 'bower');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .html files
+  gulp.watch('app/**/*.html', ['html']);
+
+  // Watch .js files
+  gulp.watch('app/js/**/*.js', ['scripts']);
+
 });
